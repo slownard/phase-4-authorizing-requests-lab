5636796eab31ad78d0bb206b3009dcbac5729d38
Bernard Legaspi <blegaspi26@gmail.com>
2022-12-23 15:16:29 -0500

    commit tree d50f629ac14de49a9a3a088a5a9eb7bb4e42b5c1

--- app/controllers/members_only_articles_controller.rb
+++ app/controllers/members_only_articles_controller.rb
@@ -1,5 +1,8 @@
 class MembersOnlyArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  #added this 
+  before_action :authorize
+
 
   def index
     articles = Article.where(is_member_only: true).includes(:user).order(created_at: :desc)
@@ -8,10 +11,19 @@ class MembersOnlyArticlesController < ApplicationController
 
   def show
     article = Article.find(params[:id])
+    #if article 
     render json: article
+    # else 
+    # render json: { error: "Not authorized" }, status: :unauthorized
+    # end
+
   end
 
   private
+
+  def authorize
+    return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
+  end
 
   def record_not_found
     render json: { error: "Article not found" }, status: :not_found
